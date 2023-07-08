98cf64f06b24fed80d8745a2e769b7f6651f2aef
JuN-front <junsei1511280530@gmail.com>
2023-07-08 22:35:40 +0900

    Atoms/EditButton

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import TEXTS from "../../variables/texts";
 import pencil from "../../../assets/svg/pencil.svg";
 
 const SampleButton = ({ onClick }) => {
