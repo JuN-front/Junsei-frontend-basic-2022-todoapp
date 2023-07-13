774840d0f0b1ed6cc6ffd76f067115947d20084e
JuN-front <junsei1511280530@gmail.com>
2023-07-13 19:24:20 +0900

    Atoms/AddTaskButton

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import TEXT from "../../../variables/texts";
+import COLOR from "../../../variables/color";
+import plus from "../../../assets/svg/plus.svg";
+
+const AddTaskButton = ({ onClick }) => {
+  return (
+    <StyledTaskButton onClick={onClick}>
+      <StyledGreenOval></StyledGreenOval>
+      <StyledTask>
+        <img src={plus}></img>
+        <StyledAddTask>タスクを追加</StyledAddTask>
+      </StyledTask>
+    </StyledTaskButton>
+  );
+};
+
+export default AddTaskButton;
+
+const StyledGreenOval = styled.div`
+  background-color: ${COLOR.GREEN_TRANSPARENT};
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  transition: all 0.2s ease;
+`;
+
+const StyledAddTask = styled.div`
+  color: ${COLOR.GREEN};
+  font-size: ${TEXT.S};
+  line-height: 20px;
+  margin-left: 10px;
+  white-space: nowrap;
+`;
+
+const StyledTask = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 2px 6px;
+`;
+
+const StyledTaskButton = styled.button`
+  border: none;
+  outline: none;
+  background: none;
+  padding: 0px;
+  position: relative;
+  cursor: pointer;
+  & > img {
+    width: 20px;
+    height: 20px;
+  }
+  &:hover {
+    ${StyledGreenOval} {
+      background-color: ${COLOR.GREEN_TRANSLUCENT};
+    }
+  }
+`;
