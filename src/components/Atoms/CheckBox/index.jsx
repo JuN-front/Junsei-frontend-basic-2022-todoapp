c3c452f9656f48162cafa181154d4aa2e98ae283
JuN-front <junsei1511280530@gmail.com>
2023-07-13 23:49:23 +0900

    Atoms/CheckBox

---
+++ src/components/Atoms/CheckBox/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import check from "../../../assets/svg/check.svg";
+
+const AddCheckBox = ({ onClick }) => {
+  return (
+    <StyledCheckBox onClick={onClick}>
+      <Styledcheck src={check}></Styledcheck>
+    </StyledCheckBox>
+  );
+};
+
+export default AddCheckBox;
+
+const StyledCheckBox = styled.button`
+  border: 2px solid ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+  border-radius: 2px;
+  background: none;
+  outline: none;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 0px;
+
+  & > img {
+    display: none;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${StyledCheck} {
+      display: block;
+    }
+  }
+`;
